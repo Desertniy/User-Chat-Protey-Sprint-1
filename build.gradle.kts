34d42dfefe41d0860439aacc2a3896872d6fbd73
Desertniy <danil-kovalevich@mail.ru>
2023-12-09 22:37:45 +0300

    third commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,6 @@ version = "0.0.1"
 
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
-    /*mainClass.set("com.example.ApplicationKt")*/
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
